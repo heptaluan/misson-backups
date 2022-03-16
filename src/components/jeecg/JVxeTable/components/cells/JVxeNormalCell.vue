ab98dc841435c80fe2fb48b3aea2a2d63282ee6f
heptaluan <vd2o@foxmail.com>
2022-03-16 17:12:02 +0800

    format

--- src/components/jeecg/JVxeTable/components/cells/JVxeNormalCell.vue
+++ src/components/jeecg/JVxeTable/components/cells/JVxeNormalCell.vue
@@ -1,42 +1,36 @@
 <template>
-  <reload-effect
-    :vNode="innerValue"
-    :effect="reloadEffect"
-    @effect-end="handleEffectEnd"
-  />
+  <reload-effect :vNode="innerValue" :effect="reloadEffect" @effect-end="handleEffectEnd" />
 </template>
 
 <script>
-  import ReloadEffect from './ReloadEffect'
-  import JVxeCellMixins from '@/components/jeecg/JVxeTable/mixins/JVxeCellMixins'
+import ReloadEffect from './ReloadEffect'
+import JVxeCellMixins from '@/components/jeecg/JVxeTable/mixins/JVxeCellMixins'
 
-  export default {
-    name: 'JVxeNormalCell',
-    mixins: [JVxeCellMixins],
-    components: {ReloadEffect},
-    computed: {
-      reloadEffectRowKeysMap() {
-        return this.renderOptions.reloadEffectRowKeysMap
-      },
-      reloadEffect() {
-        return (this.renderOptions.reloadEffect && this.reloadEffectRowKeysMap[this.row.id]) === true
-      },
+export default {
+  name: 'JVxeNormalCell',
+  mixins: [JVxeCellMixins],
+  components: { ReloadEffect },
+  computed: {
+    reloadEffectRowKeysMap() {
+      return this.renderOptions.reloadEffectRowKeysMap
     },
-    methods: {
-      // 特效结束
-      handleEffectEnd() {
-        this.$delete(this.reloadEffectRowKeysMap, this.row.id)
-      },
+    reloadEffect() {
+      return (this.renderOptions.reloadEffect && this.reloadEffectRowKeysMap[this.row.id]) === true
     },
-    // 【组件增强】注释详见：JVxeCellMixins.js
-    enhanced: {
-      switches: {
-        editRender: false,
-      },
-    }
-  }
+  },
+  methods: {
+    // 特效结束
+    handleEffectEnd() {
+      this.$delete(this.reloadEffectRowKeysMap, this.row.id)
+    },
+  },
+  // 【组件增强】注释详见：JVxeCellMixins.js
+  enhanced: {
+    switches: {
+      editRender: false,
+    },
+  },
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
