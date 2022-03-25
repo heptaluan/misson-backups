04d45a0b2410effd87b1896d226b2357b9fd61d1
heptaluan <vd2o@foxmail.com>
2022-03-25 16:36:38 +0800

    format

--- src/components/jeecg/JTime.vue
+++ src/components/jeecg/JTime.vue
@@ -5,73 +5,72 @@
     :value="momVal"
     :format="dateFormat"
     :getCalendarContainer="getCalendarContainer"
-    @change="handleTimeChange"/>
+    @change="handleTimeChange"
+  />
 </template>
 
 <script>
-  import moment from 'moment'
-  export default {
-    name: 'JTime',
-    props: {
-      placeholder:{
-        type: String,
-        default: '',
-        required: false
-      },
-      value:{
-        type: String,
-        required: false
-      },
-      dateFormat:{
-        type: String,
-        default: 'HH:mm:ss',
-        required: false
-      },
-      readOnly:{
-        type: Boolean,
-        required: false,
-        default: false
-      },
-      disabled:{
-        type: Boolean,
-        required: false,
-        default: false
-      },
-      getCalendarContainer: {
-        type: Function,
-        default: (node) => node.parentNode
-      }
+import moment from 'moment'
+export default {
+  name: 'JTime',
+  props: {
+    placeholder: {
+      type: String,
+      default: '',
+      required: false,
     },
-    data () {
-      let timeStr = this.value;
-      return {
-        decorator:"",
-        momVal:!timeStr?null:moment(timeStr,this.dateFormat)
-      }
+    value: {
+      type: String,
+      required: false,
     },
-    watch: {
-      value (val) {
-        if(!val){
-          this.momVal = null
-        }else{
-          this.momVal = moment(val,this.dateFormat)
-        }
-      }
+    dateFormat: {
+      type: String,
+      default: 'HH:mm:ss',
+      required: false,
     },
-    methods: {
-      moment,
-      handleTimeChange(mom,timeStr){
-        this.$emit('change', timeStr);
-      }
+    readOnly: {
+      type: Boolean,
+      required: false,
+      default: false,
     },
-    //2.2新增 在组件内定义 指定父组件调用时候的传值属性和事件类型 这个牛逼
-    model: {
-      prop: 'value',
-      event: 'change'
+    disabled: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    getCalendarContainer: {
+      type: Function,
+      default: (node) => node.parentNode,
+    },
+  },
+  data() {
+    let timeStr = this.value
+    return {
+      decorator: '',
+      momVal: !timeStr ? null : moment(timeStr, this.dateFormat),
     }
-  }
+  },
+  watch: {
+    value(val) {
+      if (!val) {
+        this.momVal = null
+      } else {
+        this.momVal = moment(val, this.dateFormat)
+      }
+    },
+  },
+  methods: {
+    moment,
+    handleTimeChange(mom, timeStr) {
+      this.$emit('change', timeStr)
+    },
+  },
+  //2.2新增 在组件内定义 指定父组件调用时候的传值属性和事件类型 这个牛逼
+  model: {
+    prop: 'value',
+    event: 'change',
+  },
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
